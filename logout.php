d0d9bd82efd43b7b9572b6e1460a2a6aed89b35c
marklambd21-del <marklambd21@gmail.com>
2025-11-27 18:06:38 +0100

    Update logout.php
    sd

--- logout.php
+++ logout.php
@@ -2,6 +2,5 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: index.php");
-// This is made by Magiczny_Jasiek, and only he can sell it. If you bought it from other vacban.wtf listing that was not this one: https://vacban.wtf/vacshop/78615/ then be careful using it. It is not official.
+header("Location: index.html");
 ?>
